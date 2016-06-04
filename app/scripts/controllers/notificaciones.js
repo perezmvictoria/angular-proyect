9e11d5357187deeb70ec3585121cbe375557ed1c
Victoria Perez <perez.mvictoria@gmail.com>
2016-06-04 18:56:34 -0300

    RAC - style changes

---
+++ app/scripts/controllers/notificaciones.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yapp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of yapp
+ */
+angular.module('yapp')
+  .controller('NotificacionesCtrl', function($scope, $state) {
+
+    $scope.$state = $state;
+    $scope.titulo = 'esto es una prueba';
+
+  });
