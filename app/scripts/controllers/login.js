c9ff019295e2cbd503ff553251af3412b6770600
fperaltauy <fernando.peralta1978@outlook.com>
2016-06-18 18:04:57 -0300

    Lalala

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -12,7 +12,6 @@ angular.module('yapp')
   .controller('LoginCtrl', function($scope, $location, $http) {
 
   	$scope.mLogin = function () {
-
   		var config = {
   			headers : {
   				'Content-Type' : 'application/json; charset=utf-8'
@@ -23,11 +22,10 @@ angular.module('yapp')
 			"usuario" : $scope.usuario,
 			"contrasenia" : $scope.contrasenia
 		}
-
   		$http.post('http://192.168.1.108:5000/perfil/iniciar_sesion', data, config)
     	.success(function (data, status, headers, config) {
 			$scope.PostDataResponse = data;
-			console.log(data);
+			//console.log(data);
 			$location.path('/dashboard');
         	})
     	.error(function (data, status, header, config) {
--- app/scripts/controllers/usuarios.js
+++ app/scripts/controllers/usuarios.js
@@ -8,36 +8,34 @@
  * Controller of yapp
  */
 angular.module('yapp')
-  .controller('UsuariosCtrl', function($scope, $http) {
+  .controller('UsuariosCtrl', function($scope, $location,$http) {
+	$scope.listarUsuarios = function () {
+		var config = {
+  			headers : {
+  				'Content-Type' : 'application/json; charset=utf-8'
+  			}
+  		}  		
+		var data = {
+ 			"usuario_exec":"superrac",
+ 			"rol_exec":"super_admin"
+		}
+		var vm = this;
+		vm.midata = [];
 
-
-//    $scope.$state = $state;
-//usuario_exec
-//rol_exec
-
-  	$scope.listarUsuarios = function () {
-
-		$http.get('http://192.168.1.108:5000/usuarios/listar_usuarios', {params:{"usuario_exec":"superrac","rol_exec":"super_admin"}}).
-        success(function(data) {
-			console.log(data);
-            //$scope.greeting = data;
-        });
-
-//$http.get("/url/to/resource/", {params:{"param1": val1, "param2": val2}})
-    //.then(function (response) { /* */ })...
-
-
-  		/*$http.post('http://192.168.1.108:5000/usuarios/listar_usuarios', data, config)
-    	.success(function (data, status, headers, config) {
-			$scope.PostDataResponse = data;
-			console.log(data);
-			$location.path('/dashboard');
-        	})
+    	$http.post('http://192.168.1.108:5000/usuarios/listar_usuarios', data, config)
+    		.success(function (data, status, headers, config) {
+			//$scope.PostDataResponse = data;
+			vm.midata = data;
+			console.log(vm.midata);
+			//$location.path('/dashboard');
+			//$location.path('/usuarios');
+        })
     	.error(function (data, status, header, config) {
-        	$scope.ResponseDetails = "Data: " + data +
+	        $scope.ResponseDetails = "Data: " + data +
 				"<hr />status: " + status +
-            	"<hr />headers: " + header +
-            	"<hr />config: " + config;
-        	});*/
-    	};
-  });
+	            "<hr />headers: " + header +
+	            "<hr />config: " + config;
+	        	});
+			};
+    $scope.listarUsuarios();
+	});
