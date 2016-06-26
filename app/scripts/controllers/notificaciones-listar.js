2664a8b1bd0332695228a789ef19f47ed29bd33f
Victoria Perez <perez.mvictoria@gmail.com>
2016-06-26 17:08:35 -0300

    RAC - selectpicker

--- app/scripts/controllers/notificaciones-listar.js
+++ app/scripts/controllers/notificaciones-listar.js
@@ -3,29 +3,26 @@
 angular.module('rac')
   .controller('NotificacionesListarCtrl', function(perfilService,$scope, $state, $http) {
 
-  $scope.$state = $state;
+      $scope.$state = $state;
 
 
-  $scope.filtro = { fechaIni :'',
-                    fechaFin :'',
-                    tecnico  :'',
-                    accion   :''
-                  };
+      $scope.filtro = { fechaIni :'', fechaFin :''};
 
 
-	$scope.listarAuditoria = function () {
-    	$http.post(perfilService.getRuta()+'/auditorias/listar_auditorias', 
-    				perfilService.getData(),perfilService.getConfig())
-    		.success(function (data, status, headers, config) {    			
-				$scope.datos = data.info;        
-				return false;
-       	})
-    	.error(function (data, status, header, config) {          
-          $scope.msjerror = "No se pudo cargar la lista de usuarios";          
-          return false;
-      })
-    }
 
-    $scope.listarAuditoria();
+        $scope.listarNotificaciones = function () {
+            $http.post(perfilService.getRuta()+'/auditorias/listar_auditorias',
+                        perfilService.getData(),perfilService.getConfig())
+                .success(function (data, status, headers, config) {
+                    $scope.datos = data.info;
+                    return false;
+            })
+            .error(function (data, status, header, config) {
+              $scope.msjerror = "No se pudo cargar la lista de usuarios";
+              return false;
+          })
+        }
+
+        $scope.listarNotificaciones();
 
   });
--- app/scripts/controllers/reportes-listar.js
+++ app/scripts/controllers/reportes-listar.js
@@ -3,6 +3,24 @@
 angular.module('rac')
   .controller('ReportesListarCtrl', function(perfilService,$scope, $state) {
           $scope.msjerror = "";
+
+          $scope.filtro = { fechaIni :'',
+              fechaFin :'',
+              tecnico  :'',
+              estado   :'',};
+
+          $scope.lstTecnicos = [
+              { 'id': '1', 'nombre': 'Juan' },
+              { 'id': '2', 'nombre': 'Jose' },
+              { 'id': '3', 'nombre': 'Pepe' }
+          ];
+
+          $scope.lstEstado = [
+              { 'id': '1', 'nombre': 'Esperar' },
+              { 'id': '2', 'nombre': 'Consolidar' },
+              { 'id': '3', 'nombre': 'Descartar' }
+          ];
+
           $scope.listarReportes = function () {
 
           }
