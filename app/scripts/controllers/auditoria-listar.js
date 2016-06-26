613aea422c50f97fbec6629de389c774edde1c5a
fperaltauy <fernando.peralta1978@outlook.com>
2016-06-26 15:48:18 -0300

    1ero del domingo
    auditoria listando pero con errores. medios crear pronto.

--- app/scripts/controllers/auditoria-listar.js
+++ app/scripts/controllers/auditoria-listar.js
@@ -2,7 +2,22 @@
 
 
 angular.module('rac')
-  .controller('AuditoriasListarCtrl', function(perfilService,$scope, $state) {
+  .controller('AuditoriasListarCtrl', function(perfilService,$scope, $state,$http) {
 
     $scope.$state = $state;
+
+    $scope.listarAuditoria = function () {
+    	$http.post(perfilService.getRuta()+'/auditorias/listar_auditorias', 
+    				perfilService.getData(),perfilService.getConfig())
+    		.success(function (data, status, headers, config) {    			
+				$scope.datos = data.info;        
+				return false;
+       	})
+    	.error(function (data, status, header, config) {          
+          $scope.msjerror = "No se pudo cargar la lista de usuarios";          
+          return false;
+      })
+    }
+
+    $scope.listarAuditoria();
   });
--- app/scripts/controllers/notificaciones-listar.js
+++ app/scripts/controllers/notificaciones-listar.js
@@ -1,7 +1,22 @@
 'use strict';
 
 angular.module('rac')
-  .controller('NotificacionesListarCtrl', function(perfilService,$scope, $state) {
+  .controller('NotificacionesListarCtrl', function(perfilService,$scope, $state, $http) {
 
     $scope.$state = $state;
+
+	$scope.listarAuditoria = function () {
+    	$http.post(perfilService.getRuta()+'/auditorias/listar_auditorias', 
+    				perfilService.getData(),perfilService.getConfig())
+    		.success(function (data, status, headers, config) {    			
+				$scope.datos = data.info;        
+				return false;
+       	})
+    	.error(function (data, status, header, config) {          
+          $scope.msjerror = "No se pudo cargar la lista de usuarios";          
+          return false;
+      })
+    }
+
+    $scope.listarAuditoria();
   });
