4db1aac9303be41be028eb1599fbf943ffb81896
fperaltauy <fernando.peralta1978@outlook.com>
2016-07-06 21:38:12 -0300

    Borrar arreglado con popup

--- app/scripts/controllers/usuarios-listar.js
+++ app/scripts/controllers/usuarios-listar.js
@@ -48,8 +48,8 @@ angular.module('rac')
     $scope.eliminarUsuario = function (usuario) {
       usuarioService.setUsuario(usuario);
       //$scope.usuario_seleccionado = usuarioService.getUsuario();
-      console.log(usuario.usuario);
-      console.log($scope.usuario_seleccionado.usuario);
+      //console.log(usuario.usuario);
+      //console.log($scope.usuario_seleccionado.usuario);
       var dataPost = {
         "usuario":usuario.usuario,
         "usuario_exec":$scope.nombreUsuario,
