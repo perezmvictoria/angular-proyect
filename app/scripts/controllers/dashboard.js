c3f5264f8387a891efd3757209894f67c4c37ab1
fperaltauy <fernando.peralta1978@outlook.com>
2016-08-16 17:31:03 -0300

    Cerrar sesion

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -1,10 +1,12 @@
 'use strict';
 
 angular.module('rac')
-  .controller('DashboardCtrl', function(perfilService,usuarioService,$scope, $location,$state) {
+  .controller('DashboardCtrl', function(perfilService,usuarioService,$scope, $location,$state,$http) {
 	perfilService.validarSesion($location);
 	$scope.$state = $state;
 	$scope.usuario =  perfilService.getUsuario();
+	$scope.nombreUsuario = perfilService.getUsuario().nombre;
+    $scope.rolUsuario    = perfilService.getUsuario().rol;
 
     $scope.editarPerfil = function () {
 		usuarioService.setUsuario(perfilService.getUsuario());				
@@ -20,6 +22,29 @@ angular.module('rac')
 
     // Cerrar sesion
  	$scope.logout = function () {
+
+		var dataPost = {
+      		"usuario_exec":$scope.nombreUsuario,
+      		"rol_exec":$scope.rolUsuario
+    	} 
+
+    	$http.post(perfilService.getRuta()+'/perfil/cerrar_sesion', 
+          dataPost,perfilService.getConfig())
+      	.success(function (data, status, headers, config) {         
+        	//$scope.listaDeUsuarios = data.info;     
+      	return false;
+      	})
+      	.error(function (data, status, header, config) {
+        //$scope.msjerror = data.error;
+        //$scope.msjerror= $scope.msjerror.split(":").pop();        
+        return false;
+    })
+
+
+
+
+
+
  		perfilService.setUsuario({});
  		$location.path('/login.html');
 		return "/login.html";
